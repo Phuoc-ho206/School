8d0b8ff49ceed56733b36053b3fc5c27d0cfd79e
Ho Ly Minh Phuoc <phlminh666@gmail.com>
2026-02-27 13:36:00 +0700

    Add bai tap btrieng

---
+++ btrieng/main.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+using namespace std;
+
+bool checkw(int w)
+{
+    if (1 <= w && w <= 100)
+    {
+        return true;
+    }
+    else
+    {
+        return false;
+    }
+}
+
+int main()
+{
+    int w;
+    cout << "Enter the weight of the watermelon: ";
+    cin >> w;
+    if (checkw(w))
+    {
+        if (w % 2 == 0 && w != 2)
+        {
+            cout << "Yes" << endl;
+        }
+        else
+        {
+            cout << "No" << endl;
+        }
+    }
+    system("pause");
+    return 0;
+}
